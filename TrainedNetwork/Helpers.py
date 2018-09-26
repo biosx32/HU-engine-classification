faff0b36d1ff769ea8af1fa51f93646cebd89f41
Frantisek <biosx32@gmail.com>
2018-09-26 07:24:58 +0200

    Just Renaming and so on

--- TrainedNetwork/Helpers.py
+++ TrainedNetwork/Helpers.py
@@ -6,12 +6,14 @@ import librosa.display
 import sounddevice as sd
 from time import sleep
 
+
 def _dbg_print(*args, **kwargs):
 	if 1:
-		print("dbg: ",*args,**kwargs)
+		print("dbg: ", *args, **kwargs)
 
 
 class Sample:
+	# Wrapper class for better output
 	def __init__(self, dt):
 		for key in dt:
 			self.__dict__[key] = dt[key]
@@ -19,9 +21,8 @@ class Sample:
 	def __getitem__(self, item):
 		return self.__dict__[item]
 
-
 	def __str__(self):
-		res = "Sample(Label: {}, Rate: {}, Datalen: {}, Dur: {})".format(
+		res = "Sample(Label: {}, Rate: {}, Data len: {}, Time: {})".format(
 			self.label, self.rate, len(self.data), len(self.data)/self.rate
 		)
 
@@ -30,24 +31,22 @@ class Sample:
 	def __repr__(self):
 		return self.__str__()
 
+
 def create_sample(label, rate, data):
-	data =  {
+	# Return data to improve efficiency
+	data = {
 			'label': label,
 			'rate': rate,
 			'data': data
 		}
 
-	#use Sample object or just return data
 	return Sample(data)
 
 
-
-def get_training_sources(filename):
-	# open csv and create list of sample sources
-	SampleSources = []
+def load_sources_file(filename):
+	source_list = []
 
 	with open(filename, 'rt') as csvfile:
-
 		csv_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
 
 		# read every csv line and append to
@@ -57,72 +56,80 @@ def get_training_sources(filename):
 				# skip headers and wrong entries
 				continue
 
-
-			source = {
+			_source = {
 				'filename': row[0],
 				'start': row[1],
 				'end': row[2],
 				'label': row[3]
 			}
 
-			SampleSources.append(source)
+			source_list.append(_source)
 
-	return SampleSources
+	return source_list
 
 
-def load_audio_cache(sample_sources):
-	# create a dictionary of filename: audio_data
+def load_audio_cache(source_list, resample=8000):
 	_AudioCache = {}
 
-	for sample_src in sample_sources:
-		filename = sample_src['filename']
+	for source in source_list:
+		filename = source['filename']
 
 		if filename not in _AudioCache:
-			# downsample wav file to 8khz for same training data
-			# probably can be faster by using C++ audio converter
-			_AudioCache[filename] = librosa.load('engines.wav', sr=None)
-			#todo resample to 8kHz
+			# probably can be done faster by using low-level audio converter
+			_AudioCache[filename] = librosa.load('engines.wav', sr=resample)
 		else:
 			_dbg_print("Using cached entry for file {}".format(filename))
 
 	return _AudioCache
 
 
-def get_raw_samples(audio_cache, sample_sources):
+def load_audio_samples(audio_cache, sample_sources):
 	# get raw audio samples for every label
-	raw_samples = []
-	for sample_src in sample_sources:
-		filename = sample_src['filename']
+	_AudioSamples = []
+
+	for audio_source in sample_sources:
+		filename = audio_source['filename']
 
 		if filename not in audio_cache.keys():
-			_dbg_print("Not cached audio: {}".format(filename))
+			_dbg_print("Not cached audio file: {}".format(filename))
 			continue
 
-		audio_data, audio_samplerate = audio_cache[filename]
-		time_start = float(sample_src['start'])
-		time_end = float(sample_src['end'])
+		audio_data, audio_rate = audio_cache[filename]
 
-		#_dbg_print("sample range: ",time_start, time_end)
+		time_start = float(audio_source['start'])
+		time_end = float(audio_source['end'])
+		label = audio_source['label']
 
-		start_sample = librosa.time_to_samples(time_start, sr=audio_samplerate)
-		end_sample = librosa.time_to_samples(time_end, sr=audio_samplerate)
+		# _dbg_print("Audio Sample range: ",time_start, time_end)
 
-		cutout_data = audio_data[start_sample:end_sample]
-		label = sample_src['label']
-		sample = create_sample(label, audio_samplerate, cutout_data)
-		raw_samples.append(sample)
+		start_sample = librosa.time_to_samples(time_start, sr=audio_rate)
+		end_sample = librosa.time_to_samples(time_end, sr=audio_rate)
 
-		#_dbg_print("Playing sample: ", sample)
-		#sd.play(cutout_data, audio_samplerate*4)
-		#sd.wait()
-	return raw_samples
+		# todo Safter way to get audio part?
+		try:
+			cutout_data = audio_data[start_sample:end_sample]
+		except IndexError:
+			_dbg_print("Error in getting part of audio file: Out of Range error")
+			continue
 
-def get_shiftsample_group(raw_sample):
-	#only one down-pitch, because it sound
+		sample = create_sample(label, audio_rate, cutout_data)
+		_AudioSamples.append(sample)
+
+		# _dbg_print("Playing sample: ", sample)
+		# sd.play(cutout_data, audio_rate*4)
+		# sd.wait()
+
+	return _AudioSamples
+
+
+def _sample_to_shifted_group(raw_sample):
+	# Generate shifted audio
+	shifted_samples = []
 
 	shift_start = -0.8
 	shift_end = 0.2
 	shift_step = 0.2
+
 	shift_count = (shift_end - shift_start) / shift_step
 	print("Generating  {} shiftsamples for {} ".format(shift_count, raw_sample))
 
@@ -130,27 +137,23 @@ def get_shiftsample_group(raw_sample):
 	audio_rate = raw_sample['rate']
 	audio_label = raw_sample['label']
 
-	shifted_samples = []
-
 	shift_i = shift_start
-
 	while shift_i <= shift_end:
 		gen_data = librosa.effects.pitch_shift(audio_data, sr=audio_rate, n_steps=shift_i)
 		gen_sample = create_sample(audio_label, audio_rate, gen_data)
 
 		shifted_samples.append(gen_sample)
-
-		#print("playing shifted data..")
-		#sd.play(gen_data, audio_rate)
-		#sd.wait()
-
 		shift_i += shift_step
+
+		# print("playing shifted data..")
+		# sd.play(gen_data, audio_rate)
+		# sd.wait()
 
 	return shifted_samples
 
 
-def split_sample_by(raw_sample, seconds=1):
-	subsamples = []
+def _split_sample(raw_sample, seconds=1):
+	_SplitSamples = []
 
 	rsample_data = raw_sample['data']
 	audio_rate = raw_sample['rate']
@@ -161,7 +164,7 @@ def split_sample_by(raw_sample, seconds=1):
 	sub_count = int(0.999 +len(rsample_data)/step_size)
 
 	# iterate by X samples
-	print("Generating {} subsamples from {}".format(sub_count, label))
+	_dbg_print("Generating {} SplitSamples from {}".format(sub_count, label))
 
 	while step_counter < len(rsample_data):
 		end_step = step_counter + step_size
@@ -171,68 +174,81 @@ def split_sample_by(raw_sample, seconds=1):
 			end_step = len(rsample_data)
 
 		subsample_data = rsample_data[step_counter:end_step]
-
 		subsample = create_sample(label, audio_rate, subsample_data)
-		subsamples.append(subsample)
-
-		duration = len(subsample_data)/audio_rate
-
-		rstr = r"Created subsample from {} - S:{} E:{}, R:{}, D:{}"
-		#print(rstr.format(label, step_counter, end_step, audio_rate, duration))
+		_SplitSamples.append(subsample)
 
 		step_counter += step_size
 
-		#print("playing subsample...")
-		#sd.play(subsample_data, audio_rate)
-		#sd.wait()
+		# print("playing subsample...")
+		# sd.play(subsample_data, audio_rate)
+		# sd.wait()
 
-	#print("Playing all subsamples...")
-	#for i in subsamples:
-	#	sd.play(i['data'], i['rate'])
-	#	sd.wait()
+	# print("Playing all _SplitSamples...")
+	# for i in _SplitSamples:
+	#  	 sd.play(i['data'], i['rate'])
+	# 	 sd.wait()
 
-	return subsamples
-
-
-
-def get_splitted_samples(raw_sample_list, split_by=10):
-	splitted_samples = []
-	for rsample in raw_sample_list:
-		subsamples = split_sample_by(rsample, split_by)
-		splitted_samples.extend(subsamples)
-
-	return splitted_samples
+	return _SplitSamples
 
 
-def get_shifted_samples(raw_samples):
+def split_samples_by_time(sample_list, split_by=10):
+	split_samples = []
+
+	for sample in sample_list:
+		sub_samples = _split_sample(sample, split_by)
+		split_samples.extend(sub_samples)
+
+	return split_samples
+
+
+def generate_shifted_samples(raw_samples):
 	shifted_samples = []
+
 	for splitted in raw_samples:
-		shifted_group = get_shiftsample_group(splitted)
-		shifted_samples.extend(shifted_samples)
+		shifted_group = _sample_to_shifted_group(splitted)
+		shifted_samples.extend(shifted_group)
+
 	return shifted_samples
 
 
+# Get training sources from CSV file
+SampleSources = load_sources_file('ManualClassify.csv')
 
-SampleSources = get_training_sources('ManualClassify.csv')
-AudioCache = load_audio_cache(SampleSources)
-RawSamples = get_raw_samples(AudioCache, SampleSources)
-SplittedSamples = get_splitted_samples(RawSamples, split_by=30)
-ShiftedSamples = get_shifted_samples(SplittedSamples)
+# Load all required audio files
+# todo compare resampling success
+AudioCache = load_audio_cache(SampleSources, resample=None)
+
 
 print("-"*20,"Raw Samples", "-"*20)
-print(*RawSamples,sep='\n')
+# Generate samples from AudioCache
+RawSamples = load_audio_samples(AudioCache, SampleSources)
+print(*RawSamples, sep='\n')
 
-print("-"*20,"Splitted Samples", "-"*20)
-print(*SplittedSamples,sep='\n')
 
 print("-"*20,"Shifted Samples", "-"*20)
-print(*ShiftedSamples,sep='\n')
+# Experimental trick to create more training data by shifting audio pitch
+# todo check accuracy
+# ShiftedSamples = get_shifted_samples(RawSamples)
+# print(*ShiftedSamples,sep='\n')
+
+# Free some resources to save memory
+AudioCache = None
+
+print("-"*20,"Split Samples", "-"*20)
+# Split samples by 5 seconds to get "more" samples
+# SplittedSamples = split_samples_by_time(ShiftedSamples, split_by=5)
+SplittedSamples = split_samples_by_time(RawSamples, split_by=5)
+
+print(*SplittedSamples, sep='\n')
+
+
+# todo compare score of shiftsamples vs splitsamples
+
 
 for s in SplittedSamples:
 	sd.play(s['data'], s['rate'])
 	sd.wait()
-print(RawSamples)
-print(SplittedSamples)
+
 
 
 
